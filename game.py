c431f1e9ef4082eec0208fd3bbbc4ba83c609b7c
parkie3 <patrickconnon@outlook.com>
2024-02-22 13:52:37 +0100

    dsdsdd

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ clock = pg.time.Clock()
 screen = pg.display.set_mode((401,602))
 pg.display.set_caption("Space Shooter")
 
-#fuck 
+#fuck mig
 # Spaceship character
 ship_images = []
 for i in range(3):
